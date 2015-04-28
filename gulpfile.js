6dd16366c0b00b434bc45239f82efcb48bcdcc87
whjvenyl <encecity@gmail.com>
2015-04-28 13:58:24 +0200

    first commit

--- gulpfile.js
+++ gulpfile.js
@@ -4,26 +4,24 @@ var shell       = require('gulp-shell');
 var browserSync = require('browser-sync').create();
 var reload      = browserSync.reload;
 
-var config      = require('./grunt-maven.json');
-
 gulp.task('docs', shell.task([
  'node node_modules/jsdoc/jsdoc.js '+
-   '-c node_modules/angular-jsdoc/conf.json '+   // config file
-   '-t node_modules/angular-jsdoc/template '+    // template file
-   '-d ' + config.targetPath + '/generated '+        // output directory
-   './README.md ' +                              // to include README.md as index contents
-   '-r ' + config.projectRootPath + '/../../application/portalserver/src/main/webapp/static/fipo/lib/shared'                       // source code directory
+   '-c node_modules/jsdoc-template-angular/conf.json '+   // config file
+   '-t node_modules/jsdoc-template-angular/template '+    // template file
+   '-d ./generated '+                                     // output directory
+   './README.md ' +                                       // to include README.md as index contents
+   '-r ./src'                                             // source code directory
 ]));
 
 // Static Server + watching scss/html files
 gulp.task('serve', ['docs'], function() {
 
     browserSync.init({
-        server: config.targetPath + "/generated"
+        server: "./generated"
     });
 
-    gulp.watch([config.projectRootPath + '/../../application/portalserver/src/main/webapp/static/fipo/lib/shared/**/*.js'], ['docs']);
-    gulp.watch(config.targetPath + "/generated/*.html").on('change', reload);
+    gulp.watch(['./src/**/*.js'], ['docs']);
+    gulp.watch("./generated/*.html").on('change', reload);
 });
 
 gulp.task('default', ['docs']);
